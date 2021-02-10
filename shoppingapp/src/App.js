ab4f501fbc31dc174ebe85ea6093d560c7009708
vikassh45 <vikasmudgal4@gmail.com>
2021-02-10 23:42:39 +0530

    HandsOn4

---
+++ shoppingapp/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { OnlineShopping } from './Components/OnlineShopping';
+
+function App() {
+  return (
+    <div className="App">
+      <OnlineShopping/>
+    </div>
+  );
+}
+
+export default App;
