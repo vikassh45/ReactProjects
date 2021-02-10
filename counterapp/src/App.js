536a3b9170347251b441005552e4513520c815e6
vikassh45 <vikasmudgal4@gmail.com>
2021-02-10 23:47:55 +0530

    HandsOn5

---
+++ counterapp/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CountPeople from './Components/CountPeople';
+
+function App() {
+  return (
+    <div className="App">
+      <CountPeople />
+    </div>
+  );
+}
+
+export default App;
