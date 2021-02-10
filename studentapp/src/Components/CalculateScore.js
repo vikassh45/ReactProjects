c54fbd2341e66c5ceb96f34ff3cd17182327ee77
vikassh45 <vikasmudgal4@gmail.com>
2021-02-11 00:07:25 +0530

    HandsOn2

--- studentapp/src/Components/CalculateScore.js
+++
@@ -1,8 +0,0 @@
-const calcScore=(total,goal)=>{
-    return percentToDecimal(total/goal)
-}
-export const CalculateScore=({Name,School,total,goal})=>(
-    <div className="formatstyle">
-        <h1><font color="Brown">Student Details</font></h1>
-    </div>
-)
